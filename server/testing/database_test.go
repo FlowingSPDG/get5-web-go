b3024259dcde3bc25399ac1bc7638bcefc46a3a6
Flowing <bitoreieru@gmail.com>
2020-02-16 16:56:43 +0900

    added user related tests

--- server/testing/database_test.go
+++ server/testing/database_test.go
@@ -20,3 +20,31 @@ func TestRegisterUser(t *testing.T) {
 	}
 	t.Logf("REGISTERED USER : %v\n", user)
 }
+
+func TestGetTeamsByUser(t *testing.T) {
+	// go test -v -run TestGetTeamsByUser -args -cfg ../config.ini
+	user := &db.UserData{SteamID: "76561198072054549"} // FlowingSPDG
+	user, _, err := user.GetOrCreate()
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	teams := user.GetTeams(20)
+	t.Logf("Found %d teams\n", len(teams))
+	for k, v := range teams {
+		t.Logf("Team %d : %v\n", k, v)
+	}
+}
+
+func TestGetMatchesByUser(t *testing.T) {
+	// go test -v -run TestGetMatchesByUser -args -cfg ../config.ini
+	user := &db.UserData{SteamID: "76561198072054549"} // FlowingSPDG
+	user, _, err := user.GetOrCreate()
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	matches := user.GetRecentMatches(50)
+	t.Logf("Found %d matches\n", len(matches))
+	for k, v := range matches {
+		t.Logf("Matche %d : %v\n", k, v)
+	}
+}
